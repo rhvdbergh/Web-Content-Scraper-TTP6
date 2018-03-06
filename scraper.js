4b3b7010f651cf5f110440af34d41e9535653379
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-03-05 21:36:27 -0500

    scraper skeleton added

--- scraper.js
+++ scraper.js
@@ -1,4 +1,16 @@
 const fs = require('fs');
+const rimraf = require('rimraf'); // to remove folder with content, = command line rm -rf command
+const csv = require('csv');
+const scraper = require('website-scraper');
+
+// options for the scraper
+const scraperOptions = {
+    urls: ['http://shirts4mike.com/shirts.php'],
+    directory: './html',
+    recursive: true
+        // maxRecursiveDepth: '1',
+        // maxDepth: '1'
+}
 
 // check if there is a data folder; if not, create one
 function checkForDataFolder() {
@@ -7,5 +19,12 @@ function checkForDataFolder() {
     }
 }
 
+function scrapeSite() {
+    scraper(scraperOptions, (error, result) => {});
+}
+
+
 // INITIAL SETUP
+rimraf.sync('./html'); // remove the directory in which the scraped html will be stored
 checkForDataFolder();
+scrapeSite();
