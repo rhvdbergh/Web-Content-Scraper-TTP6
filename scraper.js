598ebb2e573142118159bdcf6786e06eee72d3d8
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-03-05 22:56:56 -0500

    scraper implemented with website-scraper

--- scraper.js
+++ scraper.js
@@ -2,14 +2,17 @@ const fs = require('fs');
 const rimraf = require('rimraf'); // to remove folder with content, = command line rm -rf command
 const csv = require('csv');
 const scraper = require('website-scraper');
+const jsdom = require('jsdom'); // create a virtaul DOM to traverse
+const { JSDOM } = jsdom; // constructor for jsdom
 
 // options for the scraper
 const scraperOptions = {
     urls: ['http://shirts4mike.com/shirts.php'],
-    directory: './html',
-    recursive: true
+    directory: './html'
+        // the following can be added to make the scraper recursive
+        // recursive: true,
         // maxRecursiveDepth: '1',
-        // maxDepth: '1'
+        // maxDepth: '1' 
 }
 
 // check if there is a data folder; if not, create one
@@ -20,11 +23,17 @@ function checkForDataFolder() {
 }
 
 function scrapeSite() {
-    scraper(scraperOptions, (error, result) => {});
+    rimraf.sync('./html'); // remove the directory in which the scraped html will be stored
+
+    scraper(scraperOptions, (error, result) => {
+
+        const dom = new JSDOM(result[0].text); // create a new DOM with jsdom
+        list = dom.window.document.getElementsByClassName('products'); // traverse the dom
+        console.log(list[0].innerHTML); // and this is the result!
+    });
 }
 
 
 // INITIAL SETUP
-rimraf.sync('./html'); // remove the directory in which the scraped html will be stored
 checkForDataFolder();
 scrapeSite();
